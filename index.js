af80d89f6d24560d32f40c71ef630689c54d244a
douglasbart <24775653-douglasbart@users.noreply.replit.com>
2023-09-24 12:52:05 +0000

    06 - Entrada de Dados

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var salario_bruto = 7500;
+const prompt = require('prompt-sync')();
+
+var nome = prompt("Digite seu nome: ");
+var salario_bruto = parseFloat(prompt("Digite o salario bruto: "));
 var ir = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - ir;
-console.log("salário líquido: R$", salario_liquido);
+console.log("salário líquido é: R$", salario_liquido);
+
+
