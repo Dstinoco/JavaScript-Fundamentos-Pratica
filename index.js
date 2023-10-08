ef45ed94c9e110f541731a8ff45175051302b1f0
Dstinoco <131923250+Dstinoco@users.noreply.github.com>
2023-10-08 13:02:19 +0000

    09 - Comando repetção

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const prompt = require('prompt-sync')();
 
-var nome = prompt("Digite seu nome: ");
-var salario_bruto = parseFloat(prompt("Digite o salario bruto: "));
+for (var i =1; i<=5; i++){
+  console.log('Pessoa', i)
 
-if (salario_bruto > 0 && salario_bruto <= 1903.98){
-  var ir = salario_bruto * 0.05;
-}else if (salario_bruto <= 2826.65){
-  var ir = salario_bruto * 0.075;
-}else { var ir = salario_bruto * 0.15;
+  var nome = prompt("Digite seu nome: ");
+  var salario_bruto = parseFloat(prompt("Digite o salario bruto: "));
+  
+  if (salario_bruto > 0 && salario_bruto <= 1903.98){
+    var ir = salario_bruto * 0.05;
+  }else if (salario_bruto <= 2826.65){
+    var ir = salario_bruto * 0.075;
+  }else { var ir = salario_bruto * 0.15;
+    
+  }
+  
+  
+  var salario_liquido = salario_bruto - ir;
+  console.log("salário líquido é: R$", salario_liquido);
+  
   
 }
-
-
-var salario_liquido = salario_bruto - ir;
-console.log("salário líquido é: R$", salario_liquido);
-
-
